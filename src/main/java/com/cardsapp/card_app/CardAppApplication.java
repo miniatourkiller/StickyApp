ce7b014775308804ba5be2f6a22f0f1586ca7960
miniatourkiller <jgathiru91@gmail.com>
2025-03-26 17:24:32 +0300

    added rsa

--- src/main/java/com/cardsapp/card_app/CardAppApplication.java
+++ src/main/java/com/cardsapp/card_app/CardAppApplication.java
@@ -2,17 +2,21 @@ package com.cardsapp.card_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.cardsapp.card_app.Services.SeederService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.security.interfaces.RSAKey;
+
 import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 @Slf4j
+@EnableConfigurationProperties({RSAKey.class})
 public class CardAppApplication implements CommandLineRunner {
 	private final SeederService seederService;
 	public static void main(String[] args) {
